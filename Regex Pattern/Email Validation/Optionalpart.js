9e64587225cca11d451c306c29d35ca9f80efdaf
Pratikviswakarma <pratikvishwakarma1812@gmail.com>
2025-03-05 10:47:33 +0530

    [Pratik] Add. Valid Email address,Ensure @ , Ensure . , Ensure optional part and close expression with optional part

---
+++ Regex Pattern/Email Validation/Optionalpart.js
@@ -0,0 +1,19 @@
+function validateEmail(email) {
+    const regex = /^abc([._+,-][a-zA-Z0-9]+)?@bridgelabz\.co(\.[a-zA-Z]{2,})?$/;
+    return regex.test(email);
+}
+
+// Test cases
+console.log(validateEmail("abc@bridgelabz.co.in"));     
+console.log(validateEmail("abc.xyz@bridgelabz.co.in")); 
+console.log(validateEmail("abc-xyz@bridgelabz.co"));    
+console.log(validateEmail("abc+xyz@bridgelabz.co.us"));  
+console.log(validateEmail("abc,xyz@bridgelabz.co.uk"));  
+console.log(validateEmail("abc_123@bridgelabz.co.in"));  
+console.log(validateEmail("abcxyz@bridgelabz.co.in"));   
+
+console.log(validateEmail("abc@bridgelabz.com"));        
+console.log(validateEmail("abc#xyz@bridgelabz.co.in")); 
+console.log(validateEmail("xyz@bridgelabz.co.in"));      
+console.log(validateEmail("abc-@bridgelabz.co.in"));    
+
